7363d55f7f09cc14979bb2e8493c7bd3d513368f
huang.peng <huang.peng@immomo.com>
2022-06-08 23:25:40 +0800

    feat: 🎸 完成trie

--- src/11.trie/Trie.ts
+++ src/11.trie/Trie.ts
@@ -1,15 +1,122 @@
+import {HashMap} from "../8.哈希表/HashMap";
+
+export class Node<V> {
+  children!: HashMap<string, Node<V>>;
+  // parent?: Node<V>;
+  value?: V;
+  character!: string;
+  word: boolean = false;
+  constructor(public parent?: Node<V>) {
+    this.children = new HashMap();
+  }
+}
+
 export class Trie<V> {
+  root?: Node<V>;
   size = 0;
-  startsWith(val: {val: string}): boolean {
-    throw new Error("");
+  constructor() {
+    this.root = new Node();
   }
-  add(k: string, v: V): V {
-    throw new Error("");
+  isEmpty() {
+    return this.size == 0;
   }
-  get(val: string): V {
-    throw new Error("");
+  clear() {
+    this.root && this.root.children.clear();
+    this.size = 0;
   }
-  remove(val: string): V {
-    throw new Error("");
+  findNode(key: string) {
+    this.keyCheck(key);
+    let node = this.root;
+    if (!node) return;
+    let len = key.length;
+
+    for (let i = 0; i < len; i++) {
+      node = node.children.get(key[i]);
+      if (!node) return;
+    }
+    return node.word ? node : undefined;
+  }
+  keyCheck(key: string) {
+    if (!key || key.length === 0) {
+      throw new Error("key must not be empty");
+    }
+  }
+  startsWith(prefix: string): boolean {
+    this.keyCheck(prefix);
+    let node = this.root;
+    if (!node) return false;
+    let len = prefix.length;
+
+    for (let i = 0; i < len; i++) {
+      if (!node) return false;
+      let cNode = node.children.get(prefix[i]);
+      if (cNode) {
+        node = cNode;
+      } else {
+        return false;
+      }
+    }
+    if (!node) return false;
+    return true;
+  }
+  add(key: string, value: V) {
+    this.keyCheck(key);
+    let node = this.root;
+    if (!node) return;
+    let len = key.length;
+    let childNode: Node<V>;
+    for (let i = 0; i < len; i++) {
+      if (!node) return;
+      let cNode = node.children.get(key[i]);
+      if (cNode) {
+        node = cNode;
+      } else {
+        childNode = new Node<V>(node);
+        childNode.character = key[i];
+        node.children.put(key[i], childNode);
+        node = childNode;
+      }
+    }
+    if (!node) return;
+    if (!node.word) {
+      node.word = true;
+      node.value = value;
+      this.size++;
+      return;
+    }
+    let oldValue = node.value;
+    node.value = value;
+    return oldValue;
+  }
+  get(key: string) {
+    let node = this.findNode(key);
+    if (!node) return;
+    return node.value;
+  }
+  contains(key: string) {
+    return !!this.findNode(key);
+  }
+  remove(str: string) {
+    let node = this.findNode(str);
+
+    if (node) {
+      this.size--;
+      let oldValue = node.value;
+      if (node && node.word) return oldValue;
+      if (node.children.isEmpty()) {
+        node.word = false;
+        node.value = undefined;
+        return oldValue;
+      } else {
+        let parent: Node<V> | undefined;
+        while ((parent = node.parent)) {
+          parent.children.remove(node.character);
+          if (parent.children.isEmpty()) break;
+          node = parent;
+        }
+      }
+      return oldValue;
+    }
+    return;
   }
 }
--- src/11.trie/test-Trie.ts
+++ src/11.trie/test-Trie.ts
@@ -1,25 +1,27 @@
 import {Trie} from "./Trie";
 
-function text1() {
+function test1() {
   let trie = new Trie();
   trie.add("cat", 1);
   trie.add("dog", 2);
   trie.add("catalog", 3);
   trie.add("cast", 4);
   trie.add("小码哥", 5);
-  console.log(trie.size == 5);
-  console.log(trie.startsWith({val: "do"}));
-  console.log(trie.startsWith({val: "c"}));
-  console.log(trie.startsWith({val: "ca"}));
-  console.log(trie.startsWith({val: "cat"}));
-  console.log(trie.startsWith({val: "cata"}));
-  console.log(!trie.startsWith({val: "hehe"}));
-  console.log(trie.get("小码哥") == 5);
-  console.log(trie.remove("cat") == 1);
-  console.log(trie.remove("catalog") == 3);
-  console.log(trie.remove("cast") == 4);
-  console.log(trie.size == 2);
-  console.log(trie.startsWith({val: "小"}));
-  console.log(trie.startsWith({val: "do"}));
-  console.log(!trie.startsWith({val: "c"}));
+  // console.log(trie.size == 5);
+  // console.log(trie.startsWith("do"));
+  // console.log(trie.startsWith("c"));
+  // console.log(trie.startsWith("ca"));
+  // console.log(trie.startsWith("cata"));
+  // console.log(trie.startsWith("hehe"));
+  // console.log(trie.contains("小码哥1"));
+  // console.log(trie.get("小码哥") == 5);
+  console.log(trie.remove("cat"), 1);
+  console.log(trie.remove("catalog"), 3);
+  console.log(trie.remove("cast"), 4);
+  console.log(trie.size, 2);
+  console.log(trie.startsWith("小"));
+  console.log(trie.startsWith("do"));
+  console.log(!trie.startsWith("c"));
 }
+
+test1();
