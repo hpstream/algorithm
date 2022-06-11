e33ad5483ec8afe1c89dc1bd55d47484ef38d4f9
huang.peng <huang.peng@immomo.com>
2022-06-11 13:19:49 +0800

    feat: 🎸 完成选择排序

---
+++ src/sort/methods/selectionSort.ts
@@ -0,0 +1,13 @@
+export function selectionSort(array: number[]) {
+  for (let end = array.length - 1; end > 0; end--) {
+    let maxIndex = 0;
+    for (let begin = 1; begin <= end; begin++) {
+      if (array[maxIndex] <= array[begin]) {
+        maxIndex = begin;
+      }
+    }
+    let tmp = array[end];
+    array[end] = tmp;
+    array[maxIndex] = tmp;
+  }
+}
