5a5ecca7740185ff7b469e26e1da699faf8a5694
huang.peng <huang.peng@immomo.com>
2022-06-15 10:50:29 +0800

    feat: 🎸 完成算法提

--- .leetcode/sort/21.合并两个有序链表.js
+++ .leetcode/sort/21.合并两个有序链表.js
@@ -18,6 +18,33 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
+  let head = new ListNode();
+  vhead = head;
+
+  let l1 = list1;
+  let l2 = list2;
+
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      let tem = l2.next;
+      head.next = l2;
+      head = head.next;
+      l2 = tem;
+    } else {
+      let tem = l1.next;
+      head.next = l1;
+      head = head.next;
+      l1 = tem;
+    }
+  }
+  if (l1) {
+    head.next = l1;
+  }
+  if (l2) {
+    head.next = l2;
+  }
+
+  return vhead.next;
 
 };
 // @lc code=end
