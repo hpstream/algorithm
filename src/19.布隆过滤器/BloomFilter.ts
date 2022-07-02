7cead41543e237227293c4a46cbd2145bd3fe83d
huang.peng <huang.peng@immomo.com>
2022-07-02 17:13:25 +0800

    feat: 🎸 完成过滤器

--- src/19.布隆过滤器/BloomFilter.ts
+++ src/19.布隆过滤器/BloomFilter.ts
@@ -1,3 +1,17 @@
+function hashCode(tem: any) {
+  let str = JSON.stringify(tem);
+  let hash = 0,
+    i,
+    chr,
+    len;
+  if (str.length === 0) return hash;
+  for (i = 0, len = str.length; i < len; i++) {
+    chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
 export class BloomFilter<T> {
   bitSize!: number;
   hashSize!: number;
@@ -20,11 +34,55 @@ export class BloomFilter<T> {
     this.bitSize = Math.floor(-(n * Math.log(p)) / (ln2 * ln2));
     this.hashSize = Math.floor((this.bitSize * ln2) / n);
 
-    this.bits = new Array((this.bitSize + this.long - 1) / this.long);
+    // this.bits = new Array(
+    //   Math.floor((this.bitSize + this.long - 1) / this.long)
+    // );
+    this.bits = [];
     // console.log(this.bitSize, this.hashSize);
   }
 
   public put(value: T) {
-    // let s = value
+    let hash1 = hashCode(value);
+    let arr = [];
+    let hash2 = hash1 >>> 16; // 无符号移动
+    for (let i = 0; i < this.hashSize; i++) {
+      let combineHash = hash1 + i * hash2;
+      if (combineHash < 0) {
+        combineHash = ~combineHash;
+      }
+      let index = combineHash & this.bitSize;
+      arr.push(index);
+      this.set(index);
+    }
+    console.log(arr);
+  }
+  contains(value: T) {
+    let hash1 = hashCode(value);
+    let arr = [];
+    let hash2 = hash1 >>> 16; // 无符号移动
+    for (let i = 0; i < this.hashSize; i++) {
+      let combineHash = hash1 + i * hash2;
+
+      if (combineHash < 0) {
+        combineHash = ~combineHash;
+      }
+      let index = combineHash & this.bitSize;
+      arr.push(index);
+      if (!this.get(index)) return false;
+    }
+    console.log(arr);
+
+    return true;
+  }
+  set(index: number) {
+    let value = this.bits[Math.floor(index / this.long)];
+    let bitValue = 1 << index % this.long;
+    this.bits[Math.floor(index / this.long)] = value | bitValue;
+    return (value & bitValue) == 0;
+  }
+  get(index: number) {
+    let value = this.bits[Math.floor(index / this.long)];
+    let bitValue = 1 << index % this.long;
+    return (value & bitValue) != 0;
   }
 }
--- src/19.布隆过滤器/test-BloomFilter.ts
+++ src/19.布隆过滤器/test-BloomFilter.ts
@@ -1,7 +1,30 @@
 import {BloomFilter} from "./BloomFilter";
 
 function test1() {
-  let bf = new BloomFilter<number>(100000000, 0.01);
+  let bf = new BloomFilter<number>(1000000, 0.01);
+
+  for (let i = 1; i <= 2; i++) {
+    bf.put(i);
+  }
+  bf.put(5);
+  console.log(bf.contains(5));
+  // console.log(bf.bits);
+  // let count = 0;
+  // for (let i = 1000001; i <= 2000000; i++) {
+  //   if (bf.contains(i)) {
+  //     count++;
+  //   }
+  // }
+  // console.log(count);
+}
+
+function test2() {
+  let bf = new BloomFilter<number>(1000000, 0.01);
+
+  bf.put(100);
+
+  console.log(bf.contains(100));
 }
 
 test1();
+// test2();
