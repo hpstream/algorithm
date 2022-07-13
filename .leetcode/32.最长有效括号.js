132fbcd8cd2efba4252308b41bd4520bb4c39fa9
huang.peng <huang.peng@immomo.com>
2022-07-13 13:47:51 +0800

    fix: 🐛 保存1

--- .leetcode/32.最长有效括号.js
+++ .leetcode/32.最长有效括号.js
@@ -9,8 +9,24 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function(s) {
+// ()()()))
 
+var longestValidParentheses = function (s) {
+  let stack = [-1];
+  let maxValidCount = 0;
+  if (s.length == 0) return maxValidCount;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "(") {
+      stack.push(i);
+    } else if (stack.length == 1) {
+      // 代表无效
+      stack[0] = i;
+    } else {
+      stack.pop();
+      // 有效结束记录下
+      maxValidCount = Math.max(maxValidCount, i - stack[stack.length - 1]);
+    }
+  }
+  return maxValidCount;
 };
 // @lc code=end
-
