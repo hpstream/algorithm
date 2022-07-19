e410e702ca451191f1c4753298f6f008102bdc84
huang.peng <huang.peng@immomo.com>
2022-07-19 09:03:26 +0800

    feat: 🎸 学习全排列

---
+++ .leetcode/dfs/17.电话号码的字母组合.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=17 lang=javascript
+ *
+ * [17] 电话号码的字母组合
+ */
+
+// @lc code=start
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+// DFS: 回溯，和减枝
+var letterCombinations = function (digits) {
+  if (!digits) return [];
+  let list = [];
+  const lettersArray = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+
+  let chars = digits;
+
+  if (chars.length == 0) return;
+  let str = [];
+
+  dfs(0, chars, str, list);
+  return list;
+
+  function dfs(idx, chars, str, list) {
+    if (idx == chars.length) {
+      list.push(str.join(""));
+      return;
+    }
+    let digit = chars[idx];
+    let letters = lettersArray[chars[idx]];
+    for (let i = 0; i < letters.length; i++) {
+      str[idx] = letters[i];
+      dfs(idx + 1, chars, str, list);
+    }
+  }
+};
+// @lc code=end
--- .leetcode/树/333.最大BST子树.js
+++ .leetcode/树/333.最大BST子树.js
@@ -4,6 +4,8 @@
  * [99] 恢复二叉搜索树
  */
 
+const {upperFirst} = require("lodash");
+
 // @lc code=start
 /**
  * Definition for a binary tree node.
@@ -24,3 +26,48 @@ var largestBSTSubtree = function (root) {
 
   return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right));
 };
+
+var largestBSTSubtree = function (root) {
+  if (!root) return 0;
+  // 判断一个树是不是二叉搜索树
+
+  return getInfo().size;
+};
+
+function getInfo(root) {
+  if (!root) return null;
+
+  let li = getInfo(root.left);
+  let ri = getInfo(root.right);
+
+  let leftBstSize = -1;
+  let rightBstSize = -1;
+  let min = root.val;
+  let max = root.val;
+  if (li == null) {
+    leftBstSize = 0;
+  } else if (li.root == root.left && root.val > li.max) {
+    leftBstSize = li.size;
+    min = li.min;
+  }
+
+  if (ri == null) {
+    rightBstSize = 0;
+  } else if (ri.root == root.left && root.val > ri.min) {
+    rightBstSize = li.size;
+    max = ri.max;
+  }
+  if (leftBstSize >= 0 && rightBstSize >= 0) {
+    return new Info(root, 1 + leftBstSize + rightBstSize, max, min);
+  }
+  if (li != null && ri != null) return li.size > ri.size ? li : ri;
+
+  return li != null ? li : ri;
+}
+
+function Info(root, size, max, min) {
+  this.size = size;
+  this.root = root;
+  this.max = max;
+  this.min = min;
+}
