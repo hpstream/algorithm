113ecc36e19cc78262f2a025e7eceab002a5e5d2
huang.peng <huang.peng@immomo.com>
2022-07-22 09:53:39 +0800

    feat: 🎸 保存代码

---
+++ .leetcode-ts/栈/42.接雨水.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=typescript
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+// trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
+
+function trap(height: number[]): number {
+  let stack: number[] = [];
+  let trapCount = 0;
+  const n = height.length;
+  for (let i = 0; i < n; i++) {
+    // let end = stack.length - 1;
+    while (stack.length > 0 && height[i] > height[stack[stack.length - 1]]) {
+      const top = stack.pop() as number;
+      if (!stack.length) {
+        break;
+      }
+      const left = stack[stack.length - 1];
+      const currWidth = i - left - 1;
+      const currHeight = Math.min(height[left], height[i]) - height[top];
+
+      trapCount += currWidth * currHeight;
+    }
+    stack.push(i);
+  }
+  return trapCount;
+}
+// @lc code=end
--- .leetcode-ts/高频题/50.pow-x-n.ts
+++ .leetcode-ts/高频题/50.pow-x-n.ts
@@ -3,10 +3,43 @@
  *
  * [50] Pow(x, n)
  */
-myPow(2, -2);
+// myPow(2, -2);
 // @lc code=start
 // 快速幂(分治)
+// myPow(2.0, -2147483648);
+// console.log(binary(2147483648));
+function binary(n: number) {
+  let res: number[] = [];
+
+  while (n) {
+    res.unshift(n % 2);
+    n = Math.floor(n / 2);
+  }
+  return res.join("");
+}
+
 function myPow(x: number, n: number): number {
+  // 2.0\n-2147483648;
+  let y = n;
+  if (n < 0) {
+    x = 1 / x;
+    y = -n;
+  }
+  let res = 1.0;
+
+  while (y > 0) {
+    if ((y & 1) == 1) {
+      res *= x;
+    }
+    x *= x;
+    y = y >>> 1;
+  }
+  // console.log(res);
+
+  return res;
+}
+
+function myPow2(x: number, n: number): number {
   if (n < 0) {
     x = 1 / x;
     n = Math.abs(n);
